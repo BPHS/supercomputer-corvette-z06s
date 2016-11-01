23a9c4de79b01f46300acaa018c1d14d4a3838df
R-GARD367 <19gardnerr@bpcsd.org>
2016-11-01 08:00:01 -0400

    Create Supercomputer.java

---
+++ Supercomputer.java
@@ -0,0 +1,17 @@
+/*Author: Ryan Gardner, Paul Bush, The Z06s
+*This is the start of a supercomputer
+*Date: Halloween, 2016
+*Desc.:Use some for loops to show how fast our computer is
+*/
+public class Supercomputer {
+    public static void main(String [] args) {
+        for (int x = 1; x <= 1000000; x++) {
+            System.out.print(x + " ");
+        }
+        System.out.println();
+        for (int x = 1; x <= 1000000; x+= 5) {
+            System.out.print(x + " ");
+        }
+        System.out.println();
+    }
+}
