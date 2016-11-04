1e9935c5b23f498d734aa91f0033fc606c05dd27
Ryan Gardner <19gardnerr@bpcsd.org>
2016-11-04 01:33:11 +0000

    Paul, I have worked on the program

--- Supercomputer.java
+++ Supercomputer.java
@@ -3,26 +3,40 @@
 *Date: Halloween, 2016
 *Desc.:Use some for loops to show how fast our computer is
 */
+import java.util.Scanner;
 public class Supercomputer {
     public static void main(String [] args) {
+        Scanner FTC = new Scanner(System.in);
+        System.out.println("Please select one of the following loops");
+        System.out.println("1.) Counting to 1000000 by 1s");
+        System.out.println("2.) Counting to 1000000 by 5s");
+        System.out.println("3.) Counting dowm to 1 from 100 by 1s");
+        System.out.println("4.) Counting to 10000 from 1000 by 10s");
+        System.out.print("Your choice: ");
+        String choice = FTC.nextLine();
         // Ryan's loops
-        for (int x = 1; x <= 1000000; x++) {
+        if (choice.equals("1")) {
+            for (int x = 1; x <= 1000000; x++) {
             System.out.print(x + " ");
         }
         System.out.println();
-        for (int x = 1; x <= 1000000; x+= 5) {
-            System.out.print(x + " ");
-        }
-        System.out.println();
-        // Paul's Loops
-        for (int i = 100; i >= 1 ; i --) { 
-            System.out.print(i + " "); 
-        }
-        System.out.println();
+        } else if (choice.equals("2")) {
+            for (int x = 1; x <= 1000000; x+= 5) {
+                System.out.print(x + " ");
+            }
+            System.out.println();
         
-        for (int i = 1000; i <= 10000; i +=10) { 
-            System.out.print(i + " "); 
+        // Paul's Loops    
+        } else if (choice.equals("3")) {
+            for (int i = 100; i >= 1 ; i --) { 
+                System.out.print(i + " "); 
+            }
+            System.out.println();
+        } else if (choice.equals("4")) {
+           for (int i = 1000; i <= 10000; i +=10) { 
+                System.out.print(i + " "); 
+            }
+            System.out.println(); 
         }
-        System.out.println();
     }
 }
